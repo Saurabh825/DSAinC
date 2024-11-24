3460526abc679dfb75f5de4437395809ec56aaa3
Saurabh Kushwah <saurabh2023@glbajajgroup.org>
2024-11-24 18:04:35 +0530

    added arrays.c

---
+++ src/arrays.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to get an array from the user
+int* getArray(int* size)
+{
+    printf("Enter the size of an array: ");
+    scanf("%d", size);
+    // Allocate memory for the array
+    int* array = (int*)malloc(*size * sizeof(int));
+    if (array == NULL)
+    {
+        printf("memory allocation failed\n");
+        exit(1);
+    }
+
+    // Get array elements from the user
+    printf("Enter %d elements: \n", *size);
+    for (int i = 0; i < *size; i++)
+    {
+         printf("Elements %d: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+    return array;
+}
+
+void printArray(int* arr, int size)
+{
+    printf("The array is:\n");
+        for (int i = 0; i < size; i++) {
+            printf("%d ", arr[i]);
+        }
+    printf("\n");
+}
