5335b80e4254f22363585c44dd93b787c2f9b1ae
Saurabh Kushwah <saurabh2023@glbajajgroup.org>
2024-11-24 18:37:47 +0530

    updated CMakeLists.txt

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "searching/searching.h"
+#include "arrays.h"
 
 int main(int argc, char *argv[])
 {
-    printf("Hello, World!");
+    int size;
+    int* arr = getArray(&size);
+    binarySearch(arr, size, 56);
+
     return EXIT_SUCCESS;
 }
+
