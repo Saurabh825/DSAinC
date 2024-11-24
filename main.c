9d57da201309d616731e202652358d598366cf34
Saurabh Kushwah <saurabh2023@glbajajgroup.org>
2024-11-24 18:42:58 +0530

    updated mainc

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "searching/searching.h"
-#include "arrays.h"
 
 int main(int argc, char *argv[])
 {
-    int size;
-    int* arr = getArray(&size);
-    binarySearch(arr, size, 56);
-
+    printf("Hello, World!");
     return EXIT_SUCCESS;
 }
-
