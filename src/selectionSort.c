8e86cd7b86dd50aceb96271e36d7b352ff250699
Saurabh Kushwah <saurabh2023@glbajajgroup.org>
2024-11-24 23:37:20 +0530

    added selectionsort .c and .h files and updated sorting.h

---
+++ src/selectionSort.c
@@ -0,0 +1,15 @@
+void selectionSort(int* arr, int size)
+{
+    for (int i = 0; i < size -1; i ++)
+    {
+        int min_idx = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[min_idx];
+        arr[min_idx] = temp;
+    }
+}
