66e0282077d0750f07f6f6491a06d6086c1a5851
Saurabh Kushwah <saurabh2023@glbajajgroup.org>
2024-12-01 07:47:16 +0530

    combined seaching and sorting in single file

--- src/sorting.c
+++ src/sorting.c
@@ -1,3 +1,18 @@
+void insertionSort(int* arr, int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[i] > key)
+        {
+            arr[j + 1 ] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
 void selectionSort(int* arr, int size)
 {
     for (int i = 0; i < size -1; i ++)
@@ -13,3 +28,4 @@ void selectionSort(int* arr, int size)
         arr[min_idx] = temp;
     }
 }
+
